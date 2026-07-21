e49fd09c182f2e04b17e56530f18798dd7391aba
faizan <md.faizan.856007@gmail.com>
2026-07-21 19:39:18 +0530

    Update index.php

--- index.php
+++ index.php
@@ -33,142 +33,7 @@ function isLoggedIn() {
 </head>
 <body>
 
-    /* SACHIN */
-   
-</style>
-
-</head>
-<body>
-
-<nav class="navbar navbar-expand-lg bg-white shadow-sm">
-<div class="container">
-
-<a class="navbar-brand" href="#">School21</a>
-
-<button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#menu">
-<span class="navbar-toggler-icon"></span>
-</button>
-
-<div class="collapse navbar-collapse" id="menu">
-
-<ul class="navbar-nav ms-auto">
-
-<li class="nav-item">
-<a class="nav-link" href="#">Home</a>
-</li>
-
-<li class="nav-item">
-<a class="nav-link" href="#">Courses</a>
-</li>
-
-<li class="nav-item">
-<a class="nav-link" href="#">About</a>
-</li>
-
-<li class="nav-item">
-<a class="nav-link" href="#">Contact</a>
-</li>
-
-</ul>
-
-<a href="#" class="btn btn-primary ms-3">Login</a>
-
-</div>
-
-</div>
-</nav>
-
-
-<section class="hero">
-
-<div class="container">
-
-<div class="row align-items-center">
-
-<div class="col-lg-6">
-
-<h1>Learning Made Simple</h1>
-
-<p>
-Build your future with online courses,
-certifications and expert mentors.
-</p>
-
-<a href="#" class="btn-main">
-Get Started
-</a>
-
-</div>
-
-<div class="col-lg-6 text-center">
-
-<img src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?w=900" class="img-fluid rounded">
-
-</div>
-
-</div>
-
-</div>
-
-</section>
-
-
-<section class="py-5">
-
-<div class="container">
-
-<div class="row g-4">
-
-<div class="col-md-4">
-
-<div class="card-box">
-
-<h3>Online Classes</h3>
-
-<p>
-Study from anywhere with live interactive classes.
-</p>
-
-</div>
-
-</div>
-
-<div class="col-md-4">
-
-<div class="card-box">
-
-<h3>Expert Teachers</h3>
-
-<p>
-Learn from industry professionals.
-</p>
-
-</div>
-
-</div>
-
-<div class="col-md-4">
-
-<div class="card-box">
-
-<h3>Certificates</h3>
-
-<p>
-Receive certificates after course completion.
-</p>
-
-</div>
-
-</div>
-
-</div>
-
-</div>
-
-</section>
-
-
- /*
+    
     <nav class="main-nav">
         <div class="container nav-inner">
             <a href="<?php echo BASE_URL; ?>/index.php" class="logo"><?php echo SITE_NAME; ?></a>
@@ -191,7 +56,7 @@ Receive certificates after course completion.
     </nav>
 </header>
  
-*/
+
  
 <footer class="site-footer">
     <div class="container footer-inner">

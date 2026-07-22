d9eea0c7f16a37d9bbc4c6a42ed4fcde43254d01
faizan <md.faizan.856007@gmail.com>
2026-07-22 19:41:51 +0530

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ function isLoggedIn() {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<link href="style.css">
+<link rel="stylesheet" href="style.css">
 <meta charset="UTF-8">
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0">

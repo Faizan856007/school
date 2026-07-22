924e355407f3f0174d82f57c553fb45a24b261a3
faizan <md.faizan.856007@gmail.com>
2026-07-22 19:47:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -22,7 +22,9 @@ function isLoggedIn() {
 <link rel="stylesheet" href="style.css">
  <script src="main.js"></script>
 
-     <link rel="stylesheet" href="style.css">
+     
+    <link rel="stylesheet"
+href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 </head>
 
     

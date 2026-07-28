2c8d3fe316b602d35f405a59a1a347badbb432c6
faizan <md.faizan.856007@gmail.com>
2026-07-28 19:30:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -168,7 +168,7 @@ and a vibrant learning environment.
 
 <div class="hero-right">
 
-    <img src="images/hero.png" alt="Green Valley School">
+    <img src="images/hero.png" width="800px" height="900" alt="Green Valley School">
 
     <!-- Floating Card 1 -->
     <div class="floating-card card-1">

378462c35d87d5db429b063d7330ca7e87dd49b4
faizan <md.faizan.856007@gmail.com>
2026-07-30 19:26:16 +0530

    Update index.php

--- index.php
+++ index.php
@@ -87,7 +87,7 @@ Login
 
 </a>
 
-<a href="#" class="apply-btn">
+<a href="apply.html" class="apply-btn">
 
 Apply Now
 
@@ -442,8 +442,7 @@ CAMPUS EXPERIENCE
 
         <!-- Right Video -->
         <div class="campus-video">
-            width="100%"
-    height="100%"
+           
 
             <iframe width="560" height="315" src="https://www.youtube.com/embed/K25Q2yDl-9Y?si=mpUrG1jiW-Z3taTn" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
  title="Campus Video"

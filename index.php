1460990d7d374cb3be7344e9ec68230de6d945ee
faizan <md.faizan.856007@gmail.com>
2026-07-30 19:27:17 +0530

    Update index.php

--- index.php
+++ index.php
@@ -445,10 +445,7 @@ CAMPUS EXPERIENCE
            
 
             <iframe width="560" height="315" src="https://www.youtube.com/embed/K25Q2yDl-9Y?si=mpUrG1jiW-Z3taTn" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
- title="Campus Video"
-    frameborder="0"
-    allow="autoplay; encrypted-media"
-    allowfullscreen>
+
         </div>
 
     </div>

b95aaadd20ce719ab4748316e02f2a87725d68f7
faizan <md.faizan.856007@gmail.com>
2026-07-30 19:17:45 +0530

    Update index.php

--- index.php
+++ index.php
@@ -443,13 +443,7 @@ CAMPUS EXPERIENCE
         <!-- Right Video -->
         <div class="campus-video">
 
-            <iframe
-                src="https://www.youtube.com/embed/YOUR_VIDEO_ID?autoplay=1&mute=1&loop=1&playlist=YOUR_VIDEO_ID&controls=1"
-                title="Campus Tour"
-                frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen>
-            </iframe>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/K25Q2yDl-9Y?si=mpUrG1jiW-Z3taTn" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 
         </div>
 
